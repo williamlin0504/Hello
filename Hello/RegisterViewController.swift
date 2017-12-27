ca837253cd446d3030d20fbb5848c63f1458b2bf
林暐論 <macterwilliam>
2017-12-27 18:35:49 +0800

    12/27

--- Hello/RegisterViewController.swift
+++ Hello/RegisterViewController.swift
@@ -9,11 +9,6 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
-    @IBOutlet var username: UITextField!
-    @IBOutlet var password: UITextField!
-    @IBOutlet var confirm: UITextField!
-    @IBOutlet var phone: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,11 +20,4 @@ class RegisterViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBAction func clearInput(_ sender: AnyObject) {        username.text = " "
-        password.text = " "
-        confirm.text = " "
-        phone.text = " "
-    }
-    
 }
